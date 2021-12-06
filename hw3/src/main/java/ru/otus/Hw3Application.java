3d0e3725edd70ef4ba0c6ad68543dfa529c7136c
SavvaMey <savva28031997@gmail.com>
2021-12-06 22:36:14 +0300

    hw3 question

--- hw3/src/main/java/ru/otus/Hw3Application.java
+++ hw3/src/main/java/ru/otus/Hw3Application.java
@@ -2,12 +2,19 @@ package ru.otus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.otus.service.TestingService;
 
 @SpringBootApplication
 public class Hw3Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Hw3Application.class, args);
+        ApplicationContext context =
+                SpringApplication.run(Hw3Application.class, args);
+        var service = context.getBean(TestingService.class);
+        service.testStudent();
     }
 
 }
---
+++ hw3/src/main/java/ru/otus/service/Localizator.java
@@ -0,0 +1,5 @@
+package ru.otus.service;
+
+public interface Localizator {
+   String getMsg(String code, String[] arg);
+}
